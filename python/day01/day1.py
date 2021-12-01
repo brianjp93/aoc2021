39ba12529063e8e2d231b64b573c5a247db5ca98
Brian Perrett <perrettbrian@gmail.com>
2021-11-30 22:30:40 -0700

    smaht

--- python/day01/day1.py
+++ python/day01/day1.py
@@ -6,9 +6,6 @@ with Path(Path(__file__).parent, 'data').open() as f:
 data = [int(x) for x in raw_data.splitlines()]
 
 increasing = [b > a for a, b in zip(data, data[1:])]
-print(increasing)
-x = [
-    sum([b, c, d]) > sum([a, b, c])
-    for a,b,c,d in zip(data, data[1:], data[2:], data[3:])
-]
+print(sum(increasing))
+x = [b > a for a,b in zip(data, data[3:])]
 print(sum(x))
