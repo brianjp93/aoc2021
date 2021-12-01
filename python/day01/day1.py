3d2329d23dd7c3218a2d41cd52659b7649cb32e0
Brian Perrett <perrettbrian@gmail.com>
2021-11-30 22:18:51 -0700

    day 1

--- python/day01/day1.py
+++ python/day01/day1.py
@@ -2,3 +2,13 @@ from pathlib import Path
 
 with Path(Path(__file__).parent, 'data').open() as f:
     raw_data = f.read()
+
+data = [int(x) for x in raw_data.splitlines()]
+
+increasing = [b > a for a, b in zip(data, data[1:])]
+print(increasing)
+x = [
+    sum([b, c, d]) > sum([a, b, c])
+    for a,b,c,d in zip(data, data[1:], data[2:], data[3:])
+]
+print(sum(x))
