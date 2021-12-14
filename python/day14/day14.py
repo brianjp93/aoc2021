06843db055c26cc1ad735e4420b3a6dec88ecf62
Brian Perrett <perrettbrian@gmail.com>
2021-12-13 23:08:36 -0700

    day 14

--- python/day14/day14.py
+++ python/day14/day14.py
@@ -4,12 +4,11 @@ import re
 with Path(Path(__file__).parent, "data").open() as f:
     RAW = f.read().strip()
 
-
 start = RAW.split('\n')[0]
 rules = {a: b for a, b in re.findall(r'(\w+) -> (\w+)', RAW)}
 
-def get_next_pairs(pairs: defaultdict):
-    new_pairs = defaultdict(int)
+def get_next_pairs(pairs: defaultdict[str, int]):
+    new_pairs: defaultdict[str, int] = defaultdict(int)
     for key, count in pairs.items():
         if len(key) == 2:
             middle_letter = rules[key]
@@ -26,9 +25,7 @@ def get_counts(pairs):
     counts = letters.values()
     return max(counts) - min(counts)
 
-
-word = start
-pairs = defaultdict(int)
+pairs: defaultdict[str, int] = defaultdict(int)
 for i in range(len(start)):
     try:
         pairs[start[i:i+2]] += 1
