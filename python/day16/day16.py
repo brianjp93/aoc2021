8b719f0694a8d0ddefa38679bcb8043a016241cf
Brian Perrett <bperrett@fusionbox.com>
2021-12-16 16:38:42 -0700

    match case doesn't help us much here

--- python/day16/day16.py
+++ python/day16/day16.py
@@ -3,15 +3,6 @@ from math import prod
 with Path(Path(__file__).parent, "data").open() as f:
     RAW = f.read().strip()
 
-convert = {
-    '0': '0000', '1': '0001', '2': '0010',
-    '3': '0011', '4': '0100', '5': '0101',
-    '6': '0110', '7': '0111', '8': '1000',
-    '9': '1001', 'A': '1010', 'B': '1011',
-    'C': '1100', 'D': '1101', 'E': '1110',
-    'F': '1111',
-}
-
 def window(data: str, n: int):
     for i in range(0, len(data), n):
         yield data[i: i + n], i + n
@@ -36,35 +27,34 @@ def parse_data(data, packet_count=float('inf')):
         versions.append(version)
         type_id = int(data[3:6], 2)
         data = data[6:]
-        match type_id:
-            case 4:
-                num = []
-                index = None
-                for bits, index in window(data, 5):
-                    num.append(bits[1:])
-                    if bits[0] == '0':
-                        break
-                out.append(int(''.join(num), 2))
-                assert index
-                data = data[index:]
-            case _:
-                if data[0] == '0':
-                    n = int(data[1:16], 2)
-                    data = data[16:]
-                    n_out, n_versions, _ = parse_data(data[:n])
-                    data = data[n:]
-                else:
-                    sub_packets = int(data[1:12], 2)
-                    data = data[12:]
-                    n_out, n_versions, data = parse_data(data, packet_count=sub_packets)
+        if type_id == 4:
+            num = []
+            index = None
+            for bits, index in window(data, 5):
+                num.append(bits[1:])
+                if bits[0] == '0':
+                    break
+            out.append(int(''.join(num), 2))
+            assert index
+            data = data[index:]
+        else:
+            if data[0] == '0':
+                n = int(data[1:16], 2)
+                data = data[16:]
+                n_out, n_versions, _ = parse_data(data[:n])
+                data = data[n:]
+            else:
+                sub_packets = int(data[1:12], 2)
+                data = data[12:]
+                n_out, n_versions, data = parse_data(data, packet_count=sub_packets)
 
-                out.append(handlers[type_id](n_out))
-                versions.extend(n_versions)
+            out.append(handlers[type_id](n_out))
+            versions.extend(n_versions)
 
     return out, versions, data
 
 
-bits = ''.join(convert[ch] for ch in RAW)
+bits = ''.join(bin(int(ch, 16))[2:].zfill(4) for ch in RAW)
 out, versions, _ = parse_data(bits)
 print(sum(versions))
 print(out[0])
