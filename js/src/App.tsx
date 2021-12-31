42c772ae7607b48931bf61ad55d3d2690a95ac42
Brian Perrett <bperrett@fusionbox.com>
2021-12-30 16:54:50 -0800

    day 2 js

--- js/src/App.tsx
+++ js/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { Day1 } from "./components/days/day1";
+import { Day2 } from "./components/days/day2";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/day/1/" element={<Day1/>} />
+        <Route path="/day/2/" element={<Day2/>} />
       </Routes>
     </div>
   );
@@ -19,6 +21,12 @@ function Home() {
     <>
       <div className="container">
         <h1>Welcome home.</h1>
+        <div className="row col">
+          <Link to='/day/1/' >Day 1</Link>
+        </div>
+        <div className="row col">
+          <Link to='/day/2/' >Day 2</Link>
+        </div>
       </div>
     </>
   );
---
+++ js/src/components/days/day2.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import { Skeleton } from "./utils/Skeleton";
+import "bootstrap/dist/css/bootstrap.css";
+import classnames from "classnames";
+
+export function Day2() {
+  return (
+    <Skeleton>
+      <Part1 />
+    </Skeleton>
+  )
+}
+
+function Part1() {
+  return (
+    <>
+      <h1>Part 1</h1>
+    </>
+  )
+}
